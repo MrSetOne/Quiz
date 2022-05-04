ccf39d75e9aa4cdcf10d2a3bb5dda9695b78f6b7
tianfanshan <stf30111994@gmail.com>
2022-05-04 16:30:08 +0200

    JSON added

---
+++ js/Quiz.js
@@ -0,0 +1,123 @@
+const preguntas = {
+    "response_code": 0,
+    "results": [
+      {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "In the game &quot;Undertale&quot;, who was Mettaton&#039;s creator?",
+        "correct_answer": "Alphys",
+        "incorrect_answers": [
+          "Undyne",
+          "Sans",
+          "Asgore"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first Maxis game to feature the fictional language &quot;Simlish&quot; was The Sims (2000).",
+        "correct_answer": "False",
+        "incorrect_answers": [
+          "True"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In Half-Life 2, if you play the zombies&#039; speech in reverse, they actually speak coherent English.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+          "False"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Sniper&#039;s SMG in Team Fortress 2, was originally intended to be the Scout&#039;s primary weapon.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+          "False"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Resident Evil 4 was originally meant to be a Nintendo GameCube exclusive.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+          "False"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "How many different notes is the tune, &quot;Song of Healing&quot;, comprised of in &quot;The Legend of Zelda: Majora&#039;s Mask&quot;?",
+        "correct_answer": "3",
+        "incorrect_answers": [
+          "4",
+          "1",
+          "6"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "When was &quot;System Shock&quot; released?",
+        "correct_answer": "1994",
+        "incorrect_answers": [
+          "1995",
+          "2000",
+          "1998"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Which company developed and published Game Dev Tycoon?",
+        "correct_answer": "Greenheart Games",
+        "incorrect_answers": [
+          "Greenland Games",
+          "The Tycoonists",
+          "MomCorp"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+          "True"
+        ]
+      },
+      {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What is the lowest amount of max health you can have in Team Fortress 2?",
+        "correct_answer": "70",
+        "incorrect_answers": [
+          "100",
+          "50",
+          "95"
+        ]
+      }
+    ]
+  }
+
+  console.log(preguntas)
+
+  const starform = document.getElementById('star-form')
+  const questionContainer = document.getElementById('question-container')
+  const questionBtn = document.getElementById('question-btn')
+
+  
