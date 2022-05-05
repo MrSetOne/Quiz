309b669274209d6f51342215256dcaf0f0767557
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-05 14:11:19 +0200

    example

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,122 +1,14 @@
 //constants of question and answer
-const preguntas = {
-    "response_code": 0,
-    "results": [{
-            "category": "Entertainment: Video Games",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "In the game &quot;Undertale&quot;, who was Mettaton&#039;s creator?",
-            "correct_answer": "Alphys",
-            "incorrect_answers": [
-                "Undyne",
-                "Sans",
-                "Asgore"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "The first Maxis game to feature the fictional language &quot;Simlish&quot; was The Sims (2000).",
-            "correct_answer": "False",
-            "incorrect_answers": [
-                "True"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "In Half-Life 2, if you play the zombies&#039; speech in reverse, they actually speak coherent English.",
-            "correct_answer": "True",
-            "incorrect_answers": [
-                "False"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "The Sniper&#039;s SMG in Team Fortress 2, was originally intended to be the Scout&#039;s primary weapon.",
-            "correct_answer": "True",
-            "incorrect_answers": [
-                "False"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "Resident Evil 4 was originally meant to be a Nintendo GameCube exclusive.",
-            "correct_answer": "True",
-            "incorrect_answers": [
-                "False"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "How many different notes is the tune, &quot;Song of Healing&quot;, comprised of in &quot;The Legend of Zelda: Majora&#039;s Mask&quot;?",
-            "correct_answer": "3",
-            "incorrect_answers": [
-                "4",
-                "1",
-                "6"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "When was &quot;System Shock&quot; released?",
-            "correct_answer": "1994",
-            "incorrect_answers": [
-                "1995",
-                "2000",
-                "1998"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "Which company developed and published Game Dev Tycoon?",
-            "correct_answer": "Greenheart Games",
-            "incorrect_answers": [
-                "Greenland Games",
-                "The Tycoonists",
-                "MomCorp"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
-            "correct_answer": "False",
-            "incorrect_answers": [
-                "True"
-            ]
-        },
-        {
-            "category": "Entertainment: Video Games",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "What is the lowest amount of max health you can have in Team Fortress 2?",
-            "correct_answer": "70",
-            "incorrect_answers": [
-                "100",
-                "50",
-                "95"
-            ]
-        }
-    ]
-}
+let questions = []
 
-console.log(preguntas)
-
-/* DECLARACIONES DE VARIABLES */
+//Download
+axios
+    .get('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple')
+    .then((res) => {
+        questions = res.data.results;
+        console.log(questions);
+    })
+    .catch((err) => console.error(err));
 
 //constants of navbar
 const btnPlay = document.getElementById('btn-play')
@@ -134,12 +26,13 @@ const submitBtn = document.getElementById('submit-btn') // Submit
 
 //constants of question page
 const pageQuestion = document.getElementById('question-page') //Page
+const questionTitle = document.getElementById('question-title') // Question
 const questionForm = document.getElementById('question-form') //
 const option1 = document.getElementById('option1')
 const option2 = document.getElementById('option2')
 const option3 = document.getElementById('option3')
 const option4 = document.getElementById('option4')
-
+console.log(questionTitle);
 //constants of leaderboard page
 const pageLeaderboard = document.getElementById('leaderboard-page')
 
@@ -187,7 +80,12 @@ function selectedOption() { //Devuelve el numero en indice
     }
 }
 
+function printQuestion() {
+    questionTitle.innerText = questions[0].question
+}
+
 questionForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(selectedOption())
+    printQuestion()
+
 })
